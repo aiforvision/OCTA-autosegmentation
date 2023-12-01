27ff6b40276b069012beb1b03af1921d707ff814
KreitnerL <linus.kreitner@tum.de>
2023-12-01 15:45:55 +0100

    docs: update configs

--- configs/experiment_configs/schedule_experiments.sh
+++ configs/experiment_configs/schedule_experiments.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-experiment=S_RA_delta_N_gamma_OCTA-500
-python /home/linus/repos/OCTA-seg/train.py --config_file /home/linus/repos/OCTA-seg/configs/config_ves_seg-${experiment}.yml --split 0 &&\
-python /home/linus/repos/OCTA-seg/train.py --config_file /home/linus/repos/OCTA-seg/configs/config_ves_seg-${experiment}.yml --split 1 &&\
-python /home/linus/repos/OCTA-seg/train.py --config_file /home/linus/repos/OCTA-seg/configs/config_ves_seg-${experiment}.yml --split 2 &&\
-python /home/linus/repos/OCTA-seg/train.py --config_file /home/linus/repos/OCTA-seg/configs/config_ves_seg-${experiment}.yml --split 3 &&\
-python /home/linus/repos/OCTA-seg/train.py --config_file /home/linus/repos/OCTA-seg/configs/config_ves_seg-${experiment}.yml --split 4 &&\
-d=($(ls -d /home/linus/repos/OCTA-seg/results/ves-seg-${experiment}/*)) &&\
+experiment=ves-seg-S_GAN_OCTA-500
+echo $experiment
+python /home/linus/repos/OCTA-seg/train.py --config_file /home/linus/repos/OCTA-seg/configs/experiment_configs/config_${experiment}.yml --split 0 --save_latest False --num_workers 16 &&\
+python /home/linus/repos/OCTA-seg/train.py --config_file /home/linus/repos/OCTA-seg/configs/experiment_configs/config_${experiment}.yml --split 1 --save_latest False --num_workers 16 &&\
+python /home/linus/repos/OCTA-seg/train.py --config_file /home/linus/repos/OCTA-seg/configs/experiment_configs/config_${experiment}.yml --split 2 --save_latest False --num_workers 16 &&\
+python /home/linus/repos/OCTA-seg/train.py --config_file /home/linus/repos/OCTA-seg/configs/experiment_configs/config_${experiment}.yml --split 3 --save_latest False --num_workers 16 &&\
+python /home/linus/repos/OCTA-seg/train.py --config_file /home/linus/repos/OCTA-seg/configs/experiment_configs/config_${experiment}.yml --split 4 --save_latest False --num_workers 16 &&\
+
+d=($(ls -d /home/linus/repos/OCTA-seg/results/${experiment}/*)) &&\
 python /home/linus/repos/OCTA-seg/validate.py --config_file ${d[0]}/config.yml &&\
 python /home/linus/repos/OCTA-seg/validate.py --config_file ${d[1]}/config.yml &&\
 python /home/linus/repos/OCTA-seg/validate.py --config_file ${d[2]}/config.yml &&\
